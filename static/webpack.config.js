74064e5fe733dc3cbdb73753f6f68cc749810be3
crayon <lijiawei@smail.nju.edu.cn>
2022-09-28 21:28:35 +0800

    :tada:

---
+++ static/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    // viewer: './src/viewer.js',
+    // filetree: './src/filetree.js',
+    main: './src/main.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    clean: true,
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
